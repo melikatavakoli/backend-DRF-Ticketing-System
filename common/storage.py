5d89bcc754621b934724e7221bc4aa748697998d
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-06 15:33:27 +0330

    Update storage.py

--- common/storage.py
+++ common/storage.py
@@ -1,14 +1,8 @@
-from datetime import datetime
 import os
-
-# def upload_to_by_date(instance, filename):
-#     today = datetime.now()
-#     timestamp = today.strftime("%Y%m%d%H%M%S")
-#     file_extension = os.path.splitext(filename)[1]
-#     new_filename = f"{timestamp}{file_extension}"
-#     return os.path.join(f"profile/{today.year}/", new_filename)
+from datetime import datetime
 
 BASE_PATH = "uploads"
+
 
 def upload_to_by_date(instance, filename):
     today = datetime.now()
