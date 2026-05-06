7b9fa6b70b39b01726510317ac7e18aed997504d
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-06 15:37:34 +0330

    Update apps.py

--- ticket/apps.py
+++ ticket/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class TicketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ticket'
-    verbose_name = '13-ticket'
+    verbose_name = '01-ticket'
